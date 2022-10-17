589f0b6995adc9f0d935defc3a2b5462fa85f0e5
MariliaAlmeida <77860877+MariliaAlmeida@users.noreply.github.com>
2022-10-17 13:43:36 -0300

    Delete Paginas directory

--- Paginas/views.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic import TemplateView
-
-# Create your views here.
-class PaginaInicial(TemplateView):
-    template_name = "Paginas/index.html"
-
-
-class SobreView(TemplateView):
-    template_name = "Paginas/sobre.html"
